6ed42284a10efca65c76f55d56a09ae9300ddb19
Zemlo <45403423+Zemlo@users.noreply.github.com>
2022-04-10 15:49:57 +0100

    Ejercicio revisado, corregido y refactorizado

--- src/deposito/CCuenta.java
+++ src/deposito/CCuenta.java
@@ -11,9 +11,42 @@ public class CCuenta {
     }
 
     public CCuenta(String nom, String cue, double sal, double tipo) {
-        nombre = nom;
-        cuenta = cue;
-        saldo = sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+        setTipoInteres(tipo);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 
     public double estado() {
@@ -21,19 +54,38 @@ public class CCuenta {
     }
 
     public void ingresar(double cantidad) throws Exception {
-        if (cantidad < 0) {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        }
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception {
-        if (cantidad <= 0) {
+        if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
-        }
-        if (estado() < cantidad) {
+        if (estado() < cantidad)
             throw new Exception("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    public void operativa_cuenta() {
+        float cantidad;
+
+        System.out.println("El saldo actual es: " + estado());
+
+        try {
+            cantidad = 2300;
+            System.out.println("Retirado de cuenta la cantidad de "+cantidad);
+            retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
         }
-        saldo = saldo - cantidad;
+        try {
+            cantidad = 695;
+            System.out.println("Ingreso en cuenta de "+cantidad);
+            ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+        System.out.println("El saldo actual es: " + estado());
     }
 }
